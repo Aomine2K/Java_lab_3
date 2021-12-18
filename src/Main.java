d44c12d8c88c314c9cf29e6d80412ad18007a3b2
Aomine2K <75029082+Aomine2K@users.noreply.github.com>
2021-12-18 16:15:54 +0100

    4.4

--- src/Main.java
+++ src/Main.java
@@ -29,49 +29,6 @@ public class Main {
             c) przeiterować listę za pomocą pętli forEach wywołując na każdym objekcie metodę toString() (w każdej iteracji);
          */
 
-        class Osoba {
-            private String imie;
-            private String nazwisko;
-            private Integer indeks;
 
-            public Osoba(String imie, String nazwisko, Integer indeks) {
-                this.imie = imie;
-                this.nazwisko = nazwisko;
-                this.indeks = indeks;
-            }
-
-            public String getImie() {
-                return imie;
-            }
-
-            public String getNazwisko() {
-                return nazwisko;
-            }
-
-            public Integer getIndeks() {
-                return indeks;
-            }
-
-            public void setImie(String imie) {
-                this.imie = imie;
-            }
-
-            public void setNazwisko(String nazwisko) {
-                this.nazwisko = nazwisko;
-            }
-
-            public void setIndeks(Integer indeks) {
-                this.indeks = indeks;
-            }
-
-            @Override
-            public String toString() {
-                return "" +
-                        "imie=" + imie +
-                        ", nazwisko=" + nazwisko +
-                        ", index=" + indeks +
-                        '}';
-            }
-        }
     }
 }
---
+++ src/Osoba.java
@@ -0,0 +1,46 @@
+public class Osoba {
+
+        private String imie;
+        private String nazwisko;
+        private Integer indeks;
+
+        public Osoba(String imie, String nazwisko, Integer indeks) {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.indeks = indeks;
+        }
+
+        public String getImie() {
+            return imie;
+        }
+
+        public String getNazwisko() {
+            return nazwisko;
+        }
+
+        public Integer getIndeks() {
+            return indeks;
+        }
+
+        public void setImie(String imie) {
+            this.imie = imie;
+        }
+
+        public void setNazwisko(String nazwisko) {
+            this.nazwisko = nazwisko;
+        }
+
+        public void setIndeks(Integer indeks) {
+            this.indeks = indeks;
+        }
+
+        @Override
+        public String toString() {
+            return "" +
+                    "imie=" + imie +
+                    ", nazwisko=" + nazwisko +
+                    ", index=" + indeks +
+                    '}';
+        }
+    }
+
