61a30fcdb2fb3f76ee6c2f02954f4ca199c46f3f
Aomine2K <75029082+Aomine2K@users.noreply.github.com>
2021-12-18 16:22:37 +0100

    4.5

---
+++ src/Student.java
@@ -0,0 +1,19 @@
+public class Student {
+
+    /* zad.2
+            a) Stworzyć klasę Student z polami prywatnymi
+            - Osoba osoba,
+            - WydzialEnum wydzial;
+            b) stworzyć enum WydzialEnum (w nowym pliku) z kilkoma polami,
+            c) stworzyc konstruktory, gettery i settery
+            d) nadpisać metodę toString aby zwracała wszystkie elementy ładnie przedstawione :)
+         */
+
+    private String imie;
+    private String nazwisko;
+    private Integer indeks;
+
+
+
+
+}
---
+++ src/Wydzial.java
@@ -0,0 +1,2 @@
+public class Wydzial {
+}
